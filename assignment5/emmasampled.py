b48a50b380db7a1be6d3a0614c0d676020713311
jgontrum <gontrum@me.com>
2017-10-02 13:51:12 +0200

    Better solution for last assignment

--- assignment5/emmasampled.py
+++ assignment5/emmasampled.py
@@ -5,7 +5,7 @@ emma = gutenberg.sents('austen-emma.txt')
 # Okay, that's not the most readable or cleanest solution, but it was fun to write :3
 sentences = [
     sentence for _, sentence in filter(
-        lambda x: str(x[0]).endswith('3')
+        lambda x: x[0] % 10 == 3
         and len(x[1]) == 4
         and x[1][1].startswith('t'),
         enumerate(emma)
